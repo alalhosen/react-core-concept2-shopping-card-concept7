ba602b416d43a034522db0d3e2bb3c7ba9c399e7
Alal Hosen <alalhosen1992620120@gmail.com>
2025-09-23 16:23:47 +0600

    css add

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ function App() {
           <h1 className="bg-gray text-2xl italic font-extrabold">Shopping Card</h1>
         </div>
         <hr />
-        <div className="main-container flex rounded-xl justify-around mt-8">
-          <div className="cards-container">
-            <h1 className="text-2xl underline">This is card</h1>
+
+        {/* card section */}
+        <div className="main-container mt-8">
+          <div className="card-container">
+            <h1 className="text-2xl">This is card</h1>
           </div>
-          <div className="cart-container underline">
+          <div className="cart-container">
             <h1 className="text-2xl">This is cart</h1>
           </div>
         </div>
