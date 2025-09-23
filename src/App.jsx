8ee99152f484814b442a9119719d65bf46fd36ea
Alal Hosen <alalhosen1992620120@gmail.com>
2025-09-23 23:11:04 +0600

    image seting

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,25 @@ function App() {
         {/* card section */}
         <div className="main-container mt-8">
           <div className="cards-container">
+
+            {/******** card container ********/}
             <div className="card">
-              <img src="" alt="" />
+              <img
+                src="https://fabrilife.com/products/6194db577ef6d-square.jpg?v=20"
+                alt=""
+              />
+              <h1>Tittle Test</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+                nesciunt commodi iure corrupti nostrum ut!
+              </p>
+              <div className="card-footer">
+                <h1>520 $</h1>
+                <button className="add-btn">Add To Cart</button>
+              </div>
             </div>
+
+            {/******** cart container ********/}
             <div className="card"></div>
           </div>
           <div className="cart-container">
