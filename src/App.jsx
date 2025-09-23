e4391ac3a789b1394b5e2f1259a0e5a85caa17d4
Alal Hosen <alalhosen1992620120@gmail.com>
2025-09-23 19:38:42 +0600

    fakeData

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ function App() {
     <>
       <div>
         <div>
-         
-          <h1 className="bg-gray text-2xl italic font-extrabold">Shopping Card</h1>
+          <h1 className="bg-gray text-2xl italic font-extrabold">
+            Shopping Card
+          </h1>
         </div>
         <hr />
 
         {/* card section */}
         <div className="main-container mt-8">
-          <div className="card-container">
-            <h1 className="text-2xl">This is card</h1>
+          <div className="cards-container">
+            <div className="card">
+              <img src="" alt="" />
+            </div>
+            <div className="card"></div>
           </div>
           <div className="cart-container">
             <h1 className="text-2xl">This is cart</h1>
