a48d7e789c813a6c1f568eaabe87725cdec82fe2
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-01 21:34:15 +0600

    tailwind css use in single cart component

--- src/SingleProduct.jsx
+++ src/SingleProduct.jsx
@@ -14,7 +14,7 @@ const SingleProduct = ({ product, handleCart }) => {
           facilis optio omnis harum aperiam?
         </p>
         <div className="card-footer">
-          <h1>{product.price} $</h1>
+          <h1 className="text-2xl font-bold mr-4">{product.price} $</h1>
           <button onClick={(e) => handleCart(product)} className="add-btn">
             Add To Cart
           </button>
